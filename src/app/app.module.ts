1b80d638c61b67cfec755696fdd0eb3782cda577
irina <irina27karj@gmail.com>
2021-03-21 13:14:01 +0300

    Complete the second milestone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import {AppComponent} from './app.component';
-import {HeroesComponent} from './heroes/heroes.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
@@ -17,5 +17,4 @@ import {HeroesComponent} from './heroes/heroes.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
