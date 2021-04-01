6e9a1d7c854e632d2eb666b9d6243a797fd7c596
Irina <irina27karj@gmail.com>
2021-04-01 21:07:48 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-// @ts-ignore
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-// @ts-ignore
-import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -20,16 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientModule,
-
-// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-// and returns simulated server responses.
-// Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,27 +5,16 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
-import { InMemoryDataService } from './in-memory-data.service';
-import {HttpClient} from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes';  // URL to web api
+  constructor(private messageService: MessageService) { }
 
-  constructor(
-    private http: HttpClient,
-    private messageService: MessageService) {
-  }
-
-  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl)
-      .pipe(
-        tap(_ => this.log('fetched heroes')),
-        catchError(this.handleError<Hero[]>('getHeroes', []))
-      );
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 
   getHero(id: number): Observable<Hero> {
@@ -34,25 +23,6 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id) as Hero;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
-  }
-
-  /** Log a HeroService message with the MessageService */
-  // tslint:disable-next-line:typedef
-  private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
-  }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  // tslint:disable-next-line:typedef
-  private handleError<T>(operation = 'operation', result?: T) {
-    // @ts-ignore
-    return (error: any): Observable<T> => {
-    };
   }
 }
 
